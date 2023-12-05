9bfa0f5356dd229df62eb26d6e5a62e3ab18bca2
Alexander <84857900+4z0t@users.noreply.github.com>
2023-12-05 16:19:07 +0300

    Добавьте файлы проекта.

---
+++ aac6/Program.cs
@@ -0,0 +1,27 @@
+﻿
+class Program
+{
+    private static void PrintStack(Stack<Token> stack)
+    {
+        var stackCopy = new Stack<Token>(stack.Reverse());
+        while (stackCopy.Count != 0)
+        {
+            Console.WriteLine(stackCopy.Pop());
+        }
+        Console.WriteLine();
+    }
+
+    public static void Main(string[] args)
+    {
+        var input = @"
+        <block>
+            <coloumn> Hello world </coloumn>
+        </block>
+            ";
+        var tokenizer = new Tokenizer();
+        var r = tokenizer.Tokenize(input);
+        var stack = new Stack<Token>(r);
+        r.Reverse();
+        PrintStack(stack);
+    }
+}
